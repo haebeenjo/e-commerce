b1fc01178a16bbee4bea73fe435ea43a89eb659e
jo haebeen <haebin1622@naver.com>
2023-02-03 10:20:35 +0900

    ejs 파일 수정

--- app.js
+++ app.js
@@ -1,25 +1,20 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
-const ejs = require('ejs');
-
-const { upload } = require('./multer');
+const path = require('path');
+const render = require('./render');
 
 const router = require('./routes');
 const app = express();
 
-app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('/api', router);
-// app.use('/', render);
-
-//ejs설정
 app.set('view engine', 'ejs');
-app.set('views', __dirname + '/views');
-app.use(express.static(__dirname + '/views')); //정적파일, 이미지파일
+app.use('/api', router);
+app.use('/', render);
+app.set('views', path.join(__dirname, './views'));
+app.use(express.static('public'));
 
 app.listen(process.env.PORT, () => {
   console.log(process.env.PORT, '포트로 서버가 열렸어요!');
 });
-
 module.exports = app;
