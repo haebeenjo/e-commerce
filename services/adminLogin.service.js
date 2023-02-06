903f9208a0e075fb5a65012d0b8939a09a823281
BONGJIN CHO <bjchomr@naver.com>
2023-02-06 21:39:23 +0900

    style: main pull 땡김

--- services/adminLogin.service.js
+++ services/adminLogin.service.js
@@ -1,29 +1,28 @@
 // services/adminLogin.service.js
 
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 
-const AdminRepository = require('../repositories/admin.repository');
+const AdminRepository = require("../repositories/admin.repository");
 
 class AdminLoginService {
-    adminRepository = new AdminRepository();
+  adminRepository = new AdminRepository();
 
-    findOne = async(email, password) => {
-        const findAdmin = await this.adminRepository.findOne(email, password);
+  findOne = async (email, password) => {
+    const findAdmin = await this.adminRepository.findOne(email, password);
 
-        return{
-            adminId: findAdmin.adminId
-        }
-    }
-    issueToken = async(adminId) => {
-        const token =jwt.sign({ adminId: adminId },
-            process.env.SECRET_KEY,
-            { expiresIn: "1d" }
-            );
+    return {
+      adminId: findAdmin.adminId,
+    };
+  };
+  issueToken = async (adminId) => {
+    const token = jwt.sign({ adminId: adminId }, process.env.SECRET_KEY, {
+      expiresIn: "1d",
+    });
 
-        return{
-            token: token 
-        }
-    }
+    return {
+      token: token,
+    };
+  };
 }
 
 module.exports = AdminLoginService;
