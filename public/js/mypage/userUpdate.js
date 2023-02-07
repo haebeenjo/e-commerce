307110245eeeefce2818215a33f0cf0b74909c0d
BONGJIN CHO <bjchomr@naver.com>
2023-02-07 11:10:06 +0900

    style: 수정 컨트롤러 수정함

--- public/js/mypage/userUpdate.js
+++ public/js/mypage/userUpdate.js
@@ -7,9 +7,9 @@ function update_user() {
   const newPassword = $("#newPassword").val();
   const confirmedPassword = $("#confirmedPassword").val();
 
-  if (!name && !address && !phone_number && !currentPassword && !newPassword && !confirmedPassword) {
-    alert("어떤 정보도 입력되지 않았습니다.");
-  } if (currentPassword != null || currentPassword == newPassword){
+  if (!name || !address || !phone_number || !currentPassword || !newPassword || !confirmedPassword) {
+    alert("정보를 모두 입력해 주세요.");
+  } if (( currentPassword == newPassword) && newPassword){
     alert('현재 비밀번호와 바꿀 비밀번호가 같습니다, 다르게 수정해주세요')
   } if (newPassword !== confirmedPassword){
     alert("새로운 비밀번호 확인이 틀렸습니다.");
@@ -30,7 +30,7 @@ function update_user() {
         location.href = "/";
       },
       error: function (error) {
-        alert("정보수정 실패");
+      
         console.log("에러이유:", error);
       },
     });
